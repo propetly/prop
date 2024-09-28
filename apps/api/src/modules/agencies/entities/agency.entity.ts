a8bfb9f644c67f905cd6218dd65a14857912f433
Nick Risinger <boozy044@gmail.com>
2024-09-28 15:46:40 +0300

    feat(agencies): update entity fields

--- apps/api/src/modules/agencies/entities/agency.entity.ts
+++ apps/api/src/modules/agencies/entities/agency.entity.ts
@@ -1,6 +1,26 @@
-import { Entity, Column } from 'typeorm';
-import { BaseEntity } from '../../../common';
+import { Entity, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 import { BaseEntity } from '~/common';
 
 @Entity()
-export class Agency extends BaseEntity {}
+export class Agency extends BaseEntity {
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  address: string;
+
+  @Column({ default: false })
+  isDeleted: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
