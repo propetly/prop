907c9d099d5c79c84151d609ad35960d31421b2e
Nick Risinger <boozy044@gmail.com>
2024-09-28 15:45:11 +0300

    refactor: update paths import

--- apps/api/src/modules/agencies/entities/agency.entity.ts
+++ apps/api/src/modules/agencies/entities/agency.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, Column } from 'typeorm';
 import { BaseEntity } from '../../../common';
+import { BaseEntity } from '~/common';
 
 @Entity()
 export class Agency extends BaseEntity {}
--- apps/api/src/modules/users/entities/user.entity.ts
+++ apps/api/src/modules/users/entities/user.entity.ts
@@ -1,5 +1,5 @@
 import { Entity, Column } from 'typeorm';
-import { BaseEntity } from '../../../common';
+import { BaseEntity } from '~/common';
 
 @Entity()
 export class User extends BaseEntity {
